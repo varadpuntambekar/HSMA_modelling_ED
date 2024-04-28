4cd6bc311666b2963d4eb330f968d8fc4293b934
varadpuntambekar <147520919+varadpuntambekar@users.noreply.github.com>
2024-04-28 11:24:47 +0100

    Completed code for one run of the trial, code is buggy

--- OOP_ED_v1.0.py
+++ OOP_ED_v1.0.py
@@ -36,7 +36,7 @@ class g (object):
     #simulation variables
     number_of_runs = 1
     warmup_time = 1440 #24 hours and 60 minutes per hour
-    run_time = 10 # mins
+    run_time = 100 # mins
 
 class ed_patient (object):
     '''
@@ -91,6 +91,18 @@ class ED_sim (object):
              })
         self.individual_level_results.set_index('UHID', inplace= True) #sets the index to UHID which is just 1,2,3,4 etc
         
+        #declaring mean variables for or KPIs to be calculated at the end of one run
+        self.Mean_Q_Rec_time = 0
+        self.Mean_Q_Nurse_time = 0
+        self.Mean_Q_ED_time = 0
+        self.Mean_Q_ACU_time = 0
+        self.Rec_utilize = 0
+        self.Nurse_utilize = 0
+        self.ED_doc_utilize = 0
+        self.ACU_doc_utilize = 0
+        
+        
+        
     def generate_ed_arrivals(self):
         while True:
             self.patient_counter += 1 #this is also the UHID of the patient
@@ -119,28 +131,83 @@ class ED_sim (object):
         end_q_rec = self.env.now
         
         #storing patient level values in patient level variables
-        patient.q_time_rec = start_q_rec - end_q_rec
+        patient.q_reception = end_q_rec - start_q_rec
+        
+        #patient goes to triage
+        self.env.process(self.triage(patient))
         
         register_time = random.triangular(0,g.mean_registeration, 2*g.mean_registeration)
         
         patient.service_reception = register_time
-        #add variables to the df
-        
-        ED_sim.add_to_df(self, patient)
+       
+     
         
         yield self.env.timeout(register_time)
     
     def triage (self, patient):
-        pass
+        start_q_nurse = self.env.now
+        with self.nurse.request() as req:
+            yield req
+        
+        end_q_nurse = self.env.now
+        
+        patient.q_nurse = end_q_nurse - start_q_nurse
+        
+        #patient goes either to ACU or to ED based on probability
+        if random.random() > 0.2: #80% chance that the patient goes to ED
+            self.env.process(self.ed_ass(patient))
+        else:
+            self.env.process(self.acu_ass(patient))
+
+        
+        triage_time = random.triangular(g.mean_triage/2, g.mean_triage, g.mean_triage*2 )
+        
+        patient.service_nurse = triage_time
+        
+        
+        
+        yield self.env.timeout(triage_time)
+        
+        
     
     def ed_ass (self, patient):
-        pass
+        start_ed_q = self.env.now
+        with self.ed_doc.request() as req:
+            yield req
+        end_ed_q = self.env.now
+        patient.q_edd_ass = end_ed_q - start_ed_q
+        
+        
+        ed_ass_time = random.triangular(g.mean_ed_ass/2, g.mean_ed_ass, g.mean_ed_ass*2)
+        patient.ed_ass_time = ed_ass_time
+        
+        
+        
+        yield self.env.timeout(ed_ass_time)
+        
+        patient.tot_system_time = self.env.now - patient.time_entered_in_system
     
+        ED_sim.add_to_df(self, patient)
+        
+        
     def acu_ass (self, patient):
+        start_acu_q = self.env.now
+        
+        with self.acu_doc.request() as req:
+            yield req
+            
+        end_acu_q = self.env.now
+        patient.q_acu_ass = end_acu_q - start_acu_q
+        
+        acu_ass_time = random.triangular(g.mean_acu_ass/2, g.mean_acu_ass, g.mean_acu_ass*2)
+        
+        patient.acu_ass_time = acu_ass_time
+        yield self.env.timeout(acu_ass_time)
         
         
+        patient.tot_system_time = self.env.now - patient.time_entered_in_system
+        ED_sim.add_to_df(self, patient)
         
-        pass    
     
     def add_to_df(self, patient):
         '''
@@ -150,7 +217,7 @@ class ED_sim (object):
         df_to_add = pd.DataFrame({
             "UHID" :[patient.id],
             "Time_entered_in_system" : [patient.time_entered_in_system],
-            "Q_time_receptionist":[patient.q_time_rec],
+            "Q_time_receptionist":[patient.q_reception],
             
             #using zeros as placeholders
             "Q_time_nurse":[patient.q_nurse],
@@ -160,26 +227,72 @@ class ED_sim (object):
             "Service_time_nurse":[patient.service_nurse],
             "Service_time_acu_doc":[patient.acu_ass_time],
             "Service_time_ed_doc":[patient.ed_ass_time],
-            "Total time in System":[0]        
+            "Total time in System":[patient.tot_system_time]        
               })
         
         df_to_add.set_index('UHID', inplace=True)
         self.individual_level_results = self.individual_level_results._append(df_to_add)
-        print(self.individual_level_results)
+        #print(self.individual_level_results)
         
         
-    def mean_calculator (self, dataframe):
+    def mean_calculator (self):
         '''
         calculates the average statistic for each individual run for all the different KPIs and maybe stores it in a global database
         '''
+        #mean queuing times
+        self.Mean_Q_Rec_time = self.individual_level_results['Q_time_receptionist'].mean()
+        self.Mean_Q_Nurse_time = self.individual_level_results['Q_time_nurse'].mean()
+        self.Mean_Q_ED_time = self.individual_level_results['Q_time_ed_doc'].mean()
+        self.Mean_Q_ACU_time = self.individual_level_results['Q_time_acu_doc'].mean()
+        
+        #%resource utilisation
+        self.Rec_utilize = self.individual_level_results[
+            'Service_time_receptionist'].sum()/g.run_time
+        self.Nurse_utilize = self.individual_level_results['Service_time_nurse'].sum()/g.run_time
+        self.ED_doc_utilize = self.individual_level_results['Service_time_ed_doc'].sum()/g.run_time
+        self.ACU_doc_utilize = self.individual_level_results['Service_time_acu_doc'].sum()/g.run_time
+    
         
     
+    def export_row_to_csv(self):
+        '''
+        Writes the results of an individual run as a row in a csv file in the desired folder
+        '''
+    
+        with open (r"C:\Users\varad\Desktop\Education Material\Mathematical Modelling\HSMA\HSMA_modelling_ED\trial_results.csv",'a') as f:
+            writer = csv.writer(f, delimiter = ',')
+            columns_headers = ["Run_Number",
+                              "Mean_Q_Rec_time",
+                              "Mean_Q_Nurse_time",
+                              "Mean_Q_ED_time",
+                              "Mean_Q_ACU_time",
+                              "Rec_%_utilize",
+                              "Nurse_%_utilize",
+                              "ED_doc_%_utilize",
+                              "ACU_doc_%_utilize"]
+            
+            writer.writerow(columns_headers)
+            row_to_add = [self.Mean_Q_Rec_time,
+                               self.Mean_Q_Nurse_time,
+                               self.Mean_Q_ED_time,
+                               self.Mean_Q_ACU_time,
+                               self.Rec_utilize,
+                               self.Nurse_utilize,
+                               self.ED_doc_utilize,
+                               self.ACU_doc_utilize]
+            writer.writerow(row_to_add)
+            
+            
     def run (self):
         '''
         suns the simulation program
         '''
         self.env.process(self.generate_ed_arrivals())
         self.env.run(g.run_time)
+        print(self.individual_level_results)
+        self.individual_level_results.to_csv(r"C:\Users\varad\Desktop\Education Material\Mathematical Modelling\HSMA\HSMA_modelling_ED\individual_results.csv")
+        self.mean_calculator()
+        #self.export_to_csv()
 
 class summary_statistics(object):
     '''
@@ -222,12 +335,24 @@ def file_opener(filename):
     '''
     pass
 
-def file_writer(filename):
+def file_opener(filename):
     '''
     Adds one row to the filename that is passed to it
     '''
-    
-    pass
+    #This is not the most compulsory function here as the above function will also create a file if it does not exist already
+    with open (r"C:\Users\varad\Desktop\Education Material\
+               Mathematical Modelling\HSMA\HSMA_modelling_ED\trial_results",'w') as f:
+        writer = csv.writer(f, delimiter = ',')
+        columns_headers = ["Run_Number",
+                          "Mean_Q_Rec_time",
+                          "Mean_Q_Nurse_time",
+                          "Mean_Q_ED_time",
+                          "Mean_Q_ACU_time",
+                          "Rec_%_utilize",
+                          "Nurse_%_utilize",
+                          "ED_doc_%_utilize",
+                          "ACU_doc_%_utilize"]
+        writer.writerow(columns_headers)
 
 
 
